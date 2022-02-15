741ef845e043f8ceef49ac71135b3d61e7f05ed7
Heinrich Reinking <hreinkinge@gmail.com>
2022-02-15 12:56:07 -0300

    add secret

--- kibana/deploy.sh
+++ kibana/deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash 
 set -ie
+kubectl apply -n it-elk -f secret.yaml
 kubectl apply -n it-elk -f svc.yaml
 kubectl apply -n it-elk -f deployment.yaml
 kubectl apply -n it-elk -f ingress.yaml
--- kibana/undeploy.sh
+++ kibana/undeploy.sh
@@ -3,3 +3,4 @@ set -ie
 kubectl delete -n it-elk -f svc.yaml
 kubectl delete -n it-elk -f deployment.yaml
 kubectl delete -n it-elk -f ingress.yaml
+kubectl delete -n it-elk -f secret.yaml
