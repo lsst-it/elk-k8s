c67a4f51fcf35fcd906d654a970bececa1afff2f
Heinrich Reinking <hreinkinge@gmail.com>
2022-02-15 12:49:30 -0300

    deploy kibana

--- kibana/deploy.sh
+++ kibana/deploy.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash 
 set -ie
-
-helm install -n it-elk kibana bitnami/kibana --set elasticsearch.host[0]=elasticsearch-master,elasticsearch.port=9200 -f values.yaml 
-helm upgrade --namespace it-elk kibana bitnami/kibana     --set elasticsearch.hosts[0]=elasticsearch-master,elasticsearch.port=9200
+kubectl apply -n it-elk -f svc.yaml
+kubectl apply -n it-elk -f deployment.yaml
+kubectl apply -n it-elk -f ingress.yaml
---
+++ kibana/undeploy.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash 
+set -ie
+kubectl delete -n it-elk -f svc.yaml
+kubectl delete -n it-elk -f deployment.yaml
+kubectl delete -n it-elk -f ingress.yaml
