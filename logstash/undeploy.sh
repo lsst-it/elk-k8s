a505fcec83879144843f7a79a507557763c70783
Heinrich Reinking <hreinkinge@gmail.com>
2022-02-15 15:20:02 -0300

    add syslog input

--- logstash/undeploy.sh
+++ logstash/undeploy.sh
@@ -4,3 +4,4 @@ kubectl delete -n it-elk -f pdb.yaml
 kubectl delete -n it-elk -f svc-headless.yaml
 kubectl delete -n it-elk -f svc.yaml
 kubectl delete -n it-elk -f statefulset.yaml
+kubectl delete -n it-elk -f configmap.yaml
