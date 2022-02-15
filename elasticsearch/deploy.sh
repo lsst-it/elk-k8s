44ef66ad52f699fa908ac84ae969aef4f176ebc8
Heinrich Reinking <hreinkinge@gmail.com>
2022-02-15 16:55:26 -0300

    update CNAME record

--- elasticsearch/deploy.sh
+++ elasticsearch/deploy.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash 
 set -ie
-kubectl create ns it-elk
 kubectl apply -n it-elk -f pdb.yaml
 kubectl apply -n it-elk -f svc-headless.yaml
 kubectl apply -n it-elk -f svc.yaml
--- elasticsearch/undeploy.sh
+++ elasticsearch/undeploy.sh
@@ -4,4 +4,3 @@ kubectl delete -n it-elk -f pdb.yaml
 kubectl delete -n it-elk -f svc-headless.yaml
 kubectl delete -n it-elk -f svc.yaml
 kubectl delete -n it-elk -f statefulset.yaml
-kubectl delete ns it-elk
